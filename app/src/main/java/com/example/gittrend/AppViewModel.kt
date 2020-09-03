cfbf4b49e6b0a541cb17aa5b58022ac9c65ff529
shivansh <helloshivanshprakash@gmail.com>
2020-09-03 23:27:43 +0530

    :sparkles: feat : add no internet handling

--- app/src/main/java/com/example/gittrend/AppViewModel.kt
+++ app/src/main/java/com/example/gittrend/AppViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.gittrend
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 
 /**
  * Created by Shivansh ON 03/09/20.
@@ -11,7 +14,15 @@ import kotlinx.coroutines.launch
 class AppViewModel @ViewModelInject constructor(private val appRepository: AppRepository) :
     ViewModel() {
 
+    val noInternetLiveData = MutableLiveData(false)
     val repoListLiveData = appRepository.getRepoListLiveData()
 
-    fun refreshFromApi() = viewModelScope.launch { appRepository.refreshDatabaseFromApi() }
+    fun refreshFromApi() = viewModelScope.launch {
+        try {
+            appRepository.refreshDatabaseFromApi()
+        } catch (exception: Exception) {
+            if (exception is UnknownHostException)
+                noInternetLiveData.value = true
+        }
+    }
 }
--- app/src/main/java/com/example/gittrend/MainActivity.kt
+++ app/src/main/java/com/example/gittrend/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.gittrend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.viewModels
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gittrend.database.Repository
@@ -16,23 +19,44 @@ class MainActivity : AppCompatActivity() {
     private val repoList = mutableListOf<Repository>()
 
     private lateinit var repoAdapter: RepoAdapter
+    private lateinit var repoRecyclerView: RecyclerView
+    private lateinit var layoutNoInternet: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val repoRecyclerView = findViewById<RecyclerView>(R.id.repo_recycler_view)
+        layoutNoInternet = findViewById(R.id.layout_no_internet)
+        findViewById<Button>(R.id.button_retry).setOnClickListener { appViewModel.refreshFromApi() }
+
+        repoRecyclerView = findViewById(R.id.repo_recycler_view)
         repoRecyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         repoAdapter = RepoAdapter(repoList)
         repoRecyclerView.adapter = repoAdapter
 
-        appViewModel.refreshFromApi()
+        attachObservers()
+    }
+
+    private fun attachObservers() {
         appViewModel.repoListLiveData.observe(this) { newRepoList ->
             Log.d("repoCount", newRepoList.size.toString())
-            repoList.clear()
-            repoList.addAll(newRepoList)
-            if (this::repoAdapter.isInitialized) repoAdapter.notifyDataSetChanged()
+            if (newRepoList.isEmpty()) appViewModel.refreshFromApi()
+            else {
+                repoRecyclerView.visibility = View.VISIBLE
+                layoutNoInternet.visibility = View.GONE
+                repoList.clear()
+                repoList.addAll(newRepoList)
+                if (this::repoAdapter.isInitialized) repoAdapter.notifyDataSetChanged()
+            }
+        }
+
+        appViewModel.noInternetLiveData.observe(this) { noInternet ->
+            if (noInternet) {
+                repoRecyclerView.visibility = View.GONE
+                layoutNoInternet.visibility = View.VISIBLE
+                appViewModel.noInternetLiveData.value = false
+            }
         }
     }
 }
