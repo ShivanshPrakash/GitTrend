b1255f0ecec228479f6eba48f513a44fd284d90d
shivansh <shivansh.prakash@box8.in>
2020-09-03 14:38:26 +0530

    :sparkle: feat : setup recyclerview and implememt its adapter

--- app/src/main/java/com/example/gittrend/MainActivity.kt
+++ app/src/main/java/com/example/gittrend/MainActivity.kt
@@ -4,19 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gittrend.database.Repository
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val appViewModel by viewModels<AppViewModel>()
+    private val repoList = mutableListOf<Repository>()
+
+    private lateinit var repoAdapter: RepoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repoRecyclerView = findViewById<RecyclerView>(R.id.repo_recycler_view)
+        repoRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        repoAdapter = RepoAdapter(repoList)
+        repoRecyclerView.adapter = repoAdapter
+
         appViewModel.refreshFromApi()
-        appViewModel.repoListLiveData.observe(this) { repoList ->
-            Log.d("repoCount", repoList.size.toString())
+        appViewModel.repoListLiveData.observe(this) { newRepoList ->
+            Log.d("repoCount", newRepoList.size.toString())
+            repoList.clear()
+            repoList.addAll(newRepoList)
+            if (this::repoAdapter.isInitialized) repoAdapter.notifyDataSetChanged()
         }
     }
 }
---
+++ app/src/main/java/com/example/gittrend/RepoAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.gittrend
+
+import android.graphics.*
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gittrend.database.Repository
+
+/**
+ * Created by Shivansh ON 03/09/20.
+ */
+class RepoAdapter(private val repositoryList: List<Repository>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        RepositoryViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_repo_item, parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val repoViewHolder = holder as RepositoryViewHolder
+        val repository = repositoryList[position]
+
+        repoViewHolder.authorName.text = repository.author
+        repoViewHolder.repoName.text = repository.name
+
+        repoViewHolder.description.text = repository.description
+        repoViewHolder.language.text = repository.language
+        if (repository.languageColor != null)
+            ((repoViewHolder.langColorIndicator.background) as GradientDrawable).setColor(Color.parseColor(repository.languageColor))
+        repoViewHolder.stars.text = repository.stars.toString()
+        repoViewHolder.forks.text = repository.forks.toString()
+    }
+
+    override fun getItemCount(): Int = repositoryList.size
+
+    class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val authorName: TextView = itemView.findViewById(R.id.text_author)
+        val repoName: TextView = itemView.findViewById(R.id.text_repo_name)
+
+        val description: TextView = itemView.findViewById(R.id.text_description)
+        val langColorIndicator: View = itemView.findViewById(R.id.language_color)
+        val language: TextView = itemView.findViewById(R.id.text_language)
+        val stars: TextView = itemView.findViewById(R.id.text_stars)
+        val forks: TextView = itemView.findViewById(R.id.text_forks)
+    }
+}
